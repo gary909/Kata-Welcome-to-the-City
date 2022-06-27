2564646b26c2e1d9b4167df82678342e6ff22e63
gary <gary.white@lodgit.com>
2022-06-27 13:43:29 +0200

    code working

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 function sayHello(name, city, state) {
+    var myName = '';
+    for (var i = 0; i < name.length; i++) {
+        myName = myName + name[i] + ' '; // loop through and make the full name
+    }
+    myName = myName.slice(0, -1); // remove the exta space at end
+    return 'Hello, ' + myName + '! Welcome to ' + city + ', ' + state + '!';
 }
 
 console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')); // 'Hello, John Smith! Welcome to Phoenix, Arizona!'
